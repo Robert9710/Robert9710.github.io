593f3269715cb19f519eab5546800cc344ce3be7
Robert9710 <34442603+Robert9710@users.noreply.github.com>
2020-07-15 23:38:49 +0100

    Add files via upload

---
+++ code.js
@@ -0,0 +1,158 @@
+$(document).ready(function(){
+
+
+$("button").click(function(){
+    tet = $("#text").val();
+	//$("#mess").append(tet+"\n");
+	$("#text").val("");
+	$("pre").append(tet+"\n");
+	$("#im").attr("src", c1["img"]);
+  });
+
+var c1 = {
+	name: "",
+	level: "",
+	attribute: "",
+	mtype: "",
+	ctype: "",
+	img: "Cards/HunterDragon.png"
+}
+var c2 = {
+	name: "",
+	level: "",
+	attribute: "",
+	mtype: "",
+	ctype: "",
+	img: ""
+}
+var c3 = {
+	name: "",
+	level: "",
+	attribute: "",
+	mtype: "",
+	ctype: "",
+	img: ""
+}
+var c4 = {
+	name: "",
+	level: "",
+	attribute: "",
+	mtype: "",
+	ctype: "",
+	img: ""
+}
+/*
+var c1 = {
+	name: "",
+	level: "",
+	attribute: "",
+	mtype: "",
+	ctype: "",
+	img: ""
+}
+var c1 = {
+	name: "",
+	level: "",
+	attribute: "",
+	mtype: "",
+	ctype: "",
+	img: ""
+}
+var c1 = {
+	name: "",
+	level: "",
+	attribute: "",
+	mtype: "",
+	ctype: "",
+	img: ""
+}
+var c1 = {
+	name: "",
+	level: "",
+	attribute: "",
+	mtype: "",
+	ctype: "",
+	img: ""
+}
+var c1 = {
+	name: "",
+	level: "",
+	attribute: "",
+	mtype: "",
+	ctype: "",
+	img: ""
+}
+var c1 = {
+	name: "",
+	level: "",
+	attribute: "",
+	mtype: "",
+	ctype: "",
+	img: ""
+}
+var c1 = {
+	name: "",
+	level: "",
+	attribute: "",
+	mtype: "",
+	ctype: "",
+	img: ""
+}
+var c1 = {
+	name: "",
+	level: "",
+	attribute: "",
+	mtype: "",
+	ctype: "",
+	img: ""
+}
+var c1 = {
+	name: "",
+	level: "",
+	attribute: "",
+	mtype: "",
+	ctype: "",
+	img: ""
+}
+var c1 = {
+	name: "",
+	level: "",
+	attribute: "",
+	mtype: "",
+	ctype: "",
+	img: ""
+}
+var c1 = {
+	name: "",
+	level: "",
+	attribute: "",
+	mtype: "",
+	ctype: "",
+	img: ""
+}
+var c1 = {
+	name: "",
+	level: "",
+	attribute: "",
+	mtype: "",
+	ctype: "",
+	img: ""
+}
+var c1 = {
+	name: "",
+	level: "",
+	attribute: "",
+	mtype: "",
+	ctype: "",
+	img: ""
+}
+var c1 = {
+	name: "",
+	level: "",
+	attribute: "",
+	mtype: "",
+	ctype: "",
+	img: ""
+}
+*/
+});
