0d6e228e565a7485e6fd8169b450f0cfd90a69ff
Robert9710 <34442603+Robert9710@users.noreply.github.com>
2020-07-18 12:13:37 +0100

    Add files via upload

--- code.js
+++ code.js
@@ -2,12 +2,19 @@ $(document).ready(function(){
 
 
 $("button").click(function(){
+	
     tet = $("#text").val();
 	//$("#mess").append(tet+"\n");
 	$("#text").val("");
 	$("pre").append(tet+"\n");
-	$("#im").attr("src", c1["img"]);
-  });
+	$("#im").attr("src", "");
+	console.log("H");
+	var fs = require("fs");
+	console.log("i");
+    var text = fs.readFileSync("file.txt");
+	console.log("e");
+    print(text)
+}) 
 
 var c1 = {
 	name: "",
