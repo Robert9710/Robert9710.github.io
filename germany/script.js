a01a665ba05bd0637f5464a6ae6f8e8107e942c8
Robert9710 <34442603+Robert9710@users.noreply.github.com>
2021-01-01 15:26:54 +0000

    Add files via upload

---
+++ germany/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+	var x = screen.height;
+	
+	function hideButtons(){
+	  $('button').hide(0);
+	  $('#menu').show(1);
+	  $('#nav').mouseenter(function(){
+		$('button').show('slow');
+	  });
+	  $('#nav').mouseleave(function(){
+		$('button').hide('fast');
+		$('#menu').show(0);
+	  });
+	}
+	
+	function chooseZone(){
+	$('#nav').hide(0);
+	$('.content').hide(0);
+	$('h1').hide(0);
+	$('p').hide(0);
+	}
+	
+	chooseZone();
+	
+	$('.start_block').height(x/2.6);
+	$('.block').css("padding-top", x/8);
+	
+	$('#swb').click(function(){
+	  $('.start_block').hide(0);
+	  $('#nav').show(0);
+	  $('#sw').show(0);
+	  $('h1').show(0);
+	  $('p').show(0);
+	});
+
+    
+	
+});
